0d3becdfe16d01a648dea7db7b38cf09d691282c
echoedinvoker <echoedinvoker@gmail.com>
2023-03-27 06:09:46 +0800

    qqq

--- vue-ambassador/src/components/Nav.vue
+++ vue-ambassador/src/components/Nav.vue
@@ -2,9 +2,7 @@
   <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <!-- <li><RouterLink to="/" class="nav-link px-2 link-secondary">Frontend</RouterLink></li> -->
         <li><RouterLink to="/" class="nav-link px-2" :class="{'link-secondary': !isBackend}">Frontend</RouterLink></li>
-        <!-- <li><RouterLink to="/profile" class="nav-link px-2">Backend</RouterLink></li> -->
         <li><RouterLink to="/profile" class="nav-link px-2" :class="{'link-secondary': isBackend}">Backend</RouterLink></li>
       </ul>
 
@@ -42,3 +40,4 @@ async function logout() {
   store.setUser(null)
 }
 </script>
+
