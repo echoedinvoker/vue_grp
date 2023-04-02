11e30bedaa4d3949c45b4c1cc339091d36ae6189
echoedinvoker <echoedinvoker@gmail.com>
2023-04-02 12:32:11 +0800

    qq

---
+++ 3-vue-ambassador/33-front-sort/memo.md
@@ -0,0 +1,7 @@
+## **Implement sorting in frontend page**
+
+![Alt implement sorting in frontend page](pic/01.jpg)
+
+## **Result**
+
+![Alt result](pic/02.jpg)
--- vue-ambassador/src/views/ProductsFrontend.vue
+++ vue-ambassador/src/views/ProductsFrontend.vue
@@ -1,23 +1,45 @@
 <template>
-  <!-- <Products :products="products" @set-filters="filterProducts($event)"/> -->
-  <Products :products="filteredProducts" @set-filters="filterProducts($event)"/>
+  <Products :products="filteredProducts" :filters="filters" @set-filters="filterProducts($event)"/>
 </template>
 
 <script setup lang="ts">
 import { Filter } from '@/model/Filter';
 import { Product } from '@/model/Product';
 import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import Products from './Products.vue';
 
 const allProducts = ref<Product[]>([])
 const filteredProducts = ref<Product[]>([])
+const filters = reactive<Filter>({
+  s: '',
+  sort: ''
+})
 
 const filterProducts = (f: Filter) => {
-  filteredProducts.value = allProducts.value.filter(p => {
+  filters.s = f.s
+  filters.sort = f.sort
+
+  console.log(f)
+  // filteredProducts.value = allProducts.value.filter(p => {
+  let products = allProducts.value.filter(p => {
     return p.title.toLowerCase().indexOf(f.s.toLowerCase()) >= 0 ||
       p.description.toLowerCase().indexOf(f.s.toLowerCase()) >= 0
   })
+
+  if (filters.sort === 'asc' || filters.sort === 'desc') {
+    products.sort((a, b) => {
+      const diff = a.price - b.price
+
+      if (diff === 0) return 0
+
+      const sign = Math.abs(diff) / diff
+
+      return filters.sort === 'asc' ? sign : -sign
+    })
+  }
+
+  filteredProducts.value = products
 }
 
 onMounted(async () => {
